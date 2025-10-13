84ce13d589ed6005cc9a82c90de4b0008f98ca20
Sidney3 <sidneyarthurlevine1@hotmail.com>
2025-10-12 22:02:27 -0500

    cleaner codec

--- src/codec.py
+++ src/codec.py
@@ -1,86 +1,48 @@
 from dataclasses import dataclass
 import struct
 import datetime
-from typing import Self, List
+from typing import Self, List, Protocol
 from enum import Enum
 from typing import BinaryIO, Union
+from io import BytesIO
 
 """
 Example Usage:
 
-# To make a move
+# To read a message
+stdin_line = however_the_hell_you_read_a_line_of_stdin()
+
+
+
 """
 
-class DecodeError(Exception):
-    pass
-
-class EncodeableEnum(Enum):
-    @staticmethod
-    def FORMAT_STRING() -> str:
-        return "<c"
-
-    @staticmethod
-    def LENGTH():
-        return struct.calcsize(MessageType.FORMAT_STRING())
-
-    def encode(self):
-        return struct.pack(self.FORMAT_STRING(), self.value)
-
-    @classmethod
-    def decode(cls, data: BinaryIO):
-        return cls(struct.unpack(cls.FORMAT_STRING(), data.read(struct.calcsize(cls.FORMAT_STRING())))[0])
-
-
-
-class MessageType(EncodeableEnum):
+class MessageType(Enum):
     GAME_START = 0
     MAKE_MOVE = 1
     
-    @staticmethod
-    def FORMAT_STRING() -> str:
-        return "<B"
-
-class Player(EncodeableEnum):
+class Player(Enum):
     PLAYER_1 = b'1'
     PLAYER_2 = b'2'
-
-    @staticmethod
-    def FORMAT_STRING() -> str:
-        return "<c"
-
 
 @dataclass
 class Move:
     column: int
-
-    FORMAT_STRING = "<B"
-    LENGTH = struct.calcsize(FORMAT_STRING)
-
-    def encode(self) -> bytes:
-        return struct.pack(self.FORMAT_STRING, self.column)
-
-    @classmethod
-    def decode(cls, data: BinaryIO):
-        return cls(column = struct.unpack(cls.FORMAT_STRING, data.read(struct.calcsize(cls.FORMAT_STRING)))[0])
-
-
 
 @dataclass
 class Header:
     msg_type: MessageType
     msg_length: int
 
-    MSG_LENGTH_FORMAT_STRING = "<H"
-    LENGTH = struct.calcsize(MSG_LENGTH_FORMAT_STRING) + MessageType.LENGTH()
+    FORMAT_STRING = "<BH"
+    LENGTH = struct.calcsize(FORMAT_STRING) 
 
     def encode(self)  -> bytes:
-        return self.msg_type.encode() + struct.pack(self.MSG_LENGTH_FORMAT_STRING, self.msg_length)
+        return struct.pack(self.FORMAT_STRING, self.msg_type.value, self.msg_length)
 
     @classmethod
-    def decode(cls, data: BinaryIO) -> Self:
-        msg_type = MessageType.decode(data)
-        msg_length = struct.unpack(cls.MSG_LENGTH_FORMAT_STRING, data.read(struct.calcsize(cls.MSG_LENGTH_FORMAT_STRING)))[0]
-        return cls(msg_type=msg_type, msg_length=msg_length)
+    def decode(cls, data: bytes) -> Self:
+        (msg_type, msg_length) = struct.unpack(cls.FORMAT_STRING, data)
+        return cls(msg_type=MessageType(msg_type), msg_length=msg_length)
 
     def remaining_message_length(self) -> int:
         return self.msg_length - self.LENGTH
@@ -90,54 +52,84 @@ class MakeMove:
     header: Header
     move: Move
 
+    FORMAT_STRING = "<B"
+    LENGTH = Header.LENGTH + struct.calcsize(FORMAT_STRING)
+
     @staticmethod
     def from_move(move: Move):
-        hdr = Header(msg_type = MessageType.MAKE_MOVE, msg_length = Header.LENGTH + Move.LENGTH)
+        hdr = Header(msg_type = MessageType.MAKE_MOVE, msg_length = MakeMove.LENGTH)
         return MakeMove(header=hdr, move = move)
 
     def encode(self) -> bytes:
-        return self.header.encode() + self.move.encode()
+        return self.header.encode() + struct.pack(MakeMove.FORMAT_STRING, self.move.column)
 
     @classmethod
-    def decode(cls, header: Header, data: BinaryIO) -> Self:
-        return cls(header=header, move=Move.decode(data))
+    def decode(cls, header: Header, data: bytes) -> Self:
+        return cls(header=header, move=Move(struct.unpack(MakeMove.FORMAT_STRING, data)[0]))
 
-@dataclass
-class GameStart:
-    header: Header
+@dataclass 
+class GameParams:
     your_player: Player
     time_per_move: datetime.timedelta
     moves: List[Move] 
 
+@dataclass
+class GameStartMsg:
+    header: Header
+    params: GameParams
+
+    FIXED_FORMAT_STRING = "<cLB"
+    FIXED_LENGTH = struct.calcsize(FIXED_FORMAT_STRING)
+
     @staticmethod
-    def format_string(len: int) -> str:
-        return f"<cL{len}s"
+    def VARIABLE_FORMAT_STRING(num_moves: int):
+        return f"<{num_moves}s"
+
+    @staticmethod 
+    def from_game_params(params: GameParams):
+        msg_length = Header.LENGTH + GameStartMsg.FIXED_LENGTH + len(params.moves)
+        return GameStartMsg(
+            header = Header(msg_type=MessageType.GAME_START, msg_length=msg_length),
+            params = params
+        )
 
     def encode(self) -> bytes:
-        time_per_move = struct.pack("<L", self.time_per_move.total_seconds() * 1000)
-        moves = b"".join(m.encode() for m in self.moves)
-
-        fmt= self.format_string(len(self.moves))
-        
+        num_moves = len(self.params.moves)
         return self.header.encode() \
-            + struct.pack(fmt, self.your_player.encode(), time_per_move, moves)
+            + struct.pack(GameStartMsg.FIXED_FORMAT_STRING,
+                           self.params.your_player.value, 
+                           int(self.params.time_per_move.total_seconds() * 1000),
+                           num_moves) \
+            + struct.pack(self.VARIABLE_FORMAT_STRING(num_moves), b"".join(m.column.to_bytes(1, 'little') for m in self.params.moves))
 
     @classmethod
-    def decode(cls, header: Header, data: BinaryIO) -> Self:
-        your_player = Player.decode(data)
-        ms_per_move = struct.unpack("<L", data.read(4))[0]
+    def decode(cls, header: Header, data: bytes) -> Self:
+        buf = BytesIO(data)
+        (your_player, ms_per_move, num_moves) = struct.unpack(cls.FIXED_FORMAT_STRING, buf.read(cls.FIXED_LENGTH))
+
         time_per_move = datetime.timedelta(milliseconds = ms_per_move)
-        num_moves = struct.unpack("<B", data.read(1))[0]
-        moves: List[Move] = []
-        for _ in range(num_moves):
-            moves.append(Move.decode(data))
 
-        return cls(header=header, your_player=your_player, time_per_move = time_per_move, moves=moves)
+        variable_fmt = cls.VARIABLE_FORMAT_STRING(num_moves)
+        variable_length = struct.calcsize(variable_fmt)
+        moves: List[Move] = [Move(b) for b in struct.unpack(variable_fmt, 
+                     buf.read(variable_length))[0]]
 
-def decode_generic(data: BinaryIO) -> Union[MakeMove, GameStart]:
-    hdr = Header.decode(data)
+        return cls(header=header, params=GameParams(your_player=Player(your_player), time_per_move = time_per_move, moves=moves))
+
+
+AnyMessage = Union[Move, GameParams]
+
+class AsyncBuffer(Protocol):
+    async def read(self, len: int) -> bytes:
+        ...
+
+def decode_generic(data: bytes) -> AnyMessage:
+    buffer = BytesIO(data)
+    hdr = Header.decode(buffer.read(Header.LENGTH))
+    rest = buffer.read(hdr.remaining_message_length())
+
     match hdr.msg_type:
         case MessageType.GAME_START:
-            return GameStart.decode(hdr, data)
+            return GameStartMsg.decode(hdr, rest).params
         case MessageType.MAKE_MOVE:
-            return MakeMove.decode(hdr, data)
+            return MakeMove.decode(hdr, rest).move
--- test/test_codec.py
+++ test/test_codec.py
@@ -3,7 +3,7 @@ import struct
 import datetime
 from io import BytesIO
 from src.codec import (
-        Move, Header, MessageType, MakeMove, GameStart, decode_generic
+        Player, Move, Header, MessageType, GameParams, MakeMove, GameStartMsg, decode_generic
 )
 import sys
 
@@ -12,43 +12,37 @@ assert sys.byteorder == 'little'
 class CodecTest(unittest.TestCase):
     def test_header(self) -> None:
         hdr_raw = b'\x01\x03\x00'
-        hdr_buf = BytesIO(hdr_raw)
 
-        hdr = Header.decode(hdr_buf)
+        hdr = Header.decode(hdr_raw)
         self.assertEqual(hdr.msg_type, MessageType.MAKE_MOVE)
         self.assertEqual(hdr.msg_length, 3)
-        self.assertEqual(hdr_buf.tell(), 3)
     
         self.assertEqual(hdr.encode(), hdr_raw)
 
     def test_make_move(self) -> None:
-        move_raw = b'\x01\x03\x00\x06'
-        move_buf = BytesIO(move_raw)
+        move_raw = b'\x01\x04\x00\x06'
 
-        move = decode_generic(move_buf)
+        move = decode_generic(move_raw)
 
-        move_i_wanna_make = MakeMove.from_move(Move(4))
-        print(move_i_wanna_make.encode())
+        print(type(move))
+        assert isinstance(move, Move)
 
-        assert isinstance(move, MakeMove)
+        self.assertEqual(move, Move(6))
 
-        self.assertEqual(move.move, Move(6))
-        self.assertEqual(move_buf.tell(), len(move_raw))
-
-        self.assertEqual(move.encode(), move_raw)
+        self.assertEqual(MakeMove.from_move(move).encode(), move_raw)
 
     def test_game_start(self):
         ms_per_move = 100
         ms_per_move_encoded = struct.pack('<L', ms_per_move)
-        game_start_raw = b'\x00\x0C\x00\x31' + ms_per_move_encoded + b'\x03\x01\x00\x02'
-        game_start_buf = BytesIO(game_start_raw)
+        game_start = b'\x00\x0C\x00\x31' + ms_per_move_encoded + b'\x03\x01\x00\x02'
 
-        game_start = decode_generic(game_start_buf)
+        game_params= decode_generic(game_start)
 
-        assert isinstance(game_start, GameStart)
-        self.assertAlmostEqual(game_start.time_per_move.total_seconds() * 1000, ms_per_move, places=3)
-        self.assertEqual(game_start.moves, [Move(1), Move(0), Move(2)])
-
+        assert isinstance(game_params, GameParams)
+        self.assertAlmostEqual(game_params.time_per_move.total_seconds() * 1000, ms_per_move, places=3)
+        self.assertEqual(game_params.moves, [Move(1), Move(0), Move(2)])
+        self.assertEqual(game_params.your_player, Player.PLAYER_1)
+        self.assertEqual(GameStartMsg.from_game_params(game_params).encode(), game_start)
 
 
 if __name__ == "__main__":
